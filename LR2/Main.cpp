GOMazk/OpenLR2

commit 9d40668679a96b1429979834c51e316655e5098f

fix: set window title after creating the window
Can't set window title when there is none with DxLib-for-Linux. Related
to [1].

[1] https://github.com/dragoon2014/dxlib-for-linux/pull/4

LR2/Main.cpp
@@ -309,9 +309,6 @@ int main(int argc, char** argv) {
 		}
 #endif // _WIN32
 
-		if constexpr (!is_linux()) {
-			SetMainWindowText(LR2TITLE); // DxLib-for-Linux on Wayland segfaults on it
-		}
 		// DxLib-for-Linux only writes to stderr when writing to the log file.
 		SetOutApplicationLogValidFlag(gs.config.system.outputlog || is_linux());
 #ifdef _WIN32
@@ -333,6 +330,7 @@ int main(int argc, char** argv) {
 		}
 #endif // _WIN32
 		if (DxLib_Init() != -1) {
+			SetMainWindowText(LR2TITLE);
 			ChangeFont("", 0);
 			SetLogFontSize(14); //DXLIBVER: change this for further dxlib version
 #ifdef _WIN32

commit c68e96f61a90b0896187d4047e853dbd7ba655df

fix: set window title on Windows before creating the window
Avoids window title being "DxLib something" for a brief moment.

Fixes: 9d40668679a9 ("fix: set window title after creating the window")

LR2/Main.cpp
@@ -322,6 +322,8 @@ int main(int argc, char** argv) {
 		}
 #endif // _WIN32
 
+		// DxLib-for-Linux can only set title of an already existing window
+		if constexpr (!is_linux()) { SetMainWindowText(LR2TITLE); }
 		// DxLib-for-Linux only writes to stderr when writing to the log file.
 		SetOutApplicationLogValidFlag(gs.config.system.outputlog || is_linux());
 #ifdef _WIN32
@@ -343,7 +345,7 @@ int main(int argc, char** argv) {
 		}
 #endif // _WIN32
 		if (DxLib_Init() != -1) {
-			SetMainWindowText(LR2TITLE);
+			if constexpr (is_linux()) { SetMainWindowText(LR2TITLE); }
 			ChangeFont("", 0);
 			SetLogFontSize(14); //DXLIBVER: change this for further dxlib version
 #ifdef _WIN32