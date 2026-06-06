9ac0208715feabfbd59c862d98bde30a2d5fa293
luxurya111 <wasd33@gmail.com>
2026-06-06 23:53:12 +0900

    feat(customir): extend MethodTable with fetch/rank/ghost API types

--- LR2/LR2_customir_api.h
+++ LR2/LR2_customir_api.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <array>
+#include <vector>
 
 struct IRScoreV1 {
 	struct SONG {
@@ -105,8 +106,105 @@ enum class SendScoreStatus: int {
 	Fail,
 };
 
+struct IRRankQueryV1 {
+	IRScoreV1::SONG song{};
+	IRScoreV1::SETTINGS settings{};
+	IRScoreV1::STATE state{};
+	int exscore{};
+	int clearType{};
+};
+
+struct IRRankResultV1 {
+	int rank{};
+	int playerCount{};
+};
+
+struct IRChartStatsV1 {
+	int playerCount{};
+	std::array<int, 6> clearDistribution{};
+};
+
+struct IRLeaderboardEntryV1 {
+	int rank{};
+	int playerId{};
+	int exscore{};
+	int clearType{};
+	int pgreat{};
+	int great{};
+	int good{};
+	int bad{};
+	int poor{};
+	int maxcombo{};
+	int minbp{};
+	int playcount{};
+	int optionPacked{};
+	int keymode{};
+	std::string displayName;
+};
+
+struct IRLeaderboardResultV1 {
+	int playerCount{};
+	int myRank{};
+	int myPlayerId{};
+	std::vector<IRLeaderboardEntryV1> entries;
+};
+
+struct IRRivalChartV1 {
+	int playerId{};
+	int exscore{};
+	int clearType{};
+	int pgreat{};
+	int great{};
+	int good{};
+	int bad{};
+	int poor{};
+	int maxcombo{};
+	int minbp{};
+	int playcount{};
+	int rank{};
+	std::string displayName;
+	bool hasPlay{};
+};
+
+enum class FetchRankStatus: int {
+	Ok = 0,
+	Retry,
+	Fail,
+};
+
+struct IRProviderMetaV1 {
+	const char* webRankingChartUrlTemplate = nullptr;
+	const char* webRankingCourseUrlTemplate = nullptr;
+	const char* apiVersion = nullptr;
+};
+
+struct IRGhostQueryV1 {
+	IRScoreV1::SONG song{};
+	int mode = 0;
+	int viewerPlayerId = 0;
+	int targetPlayerId = 0;
+};
+
+struct IRGhostResultV1 {
+	std::string displayName;
+	std::string ghostData;
+	int optionDigit1{};
+	int optionDigit2{};
+	int optionDigit3{};
+	int optionDigit4{};
+	int randomSeed{};
+	int averageExscore{};
+	bool hasPlay{};
+};
+
 struct MethodTable {
 	const char*(__cdecl* GetName)() = nullptr;
 	bool(__cdecl* LoginV1)() = nullptr;
 	SendScoreStatus(__cdecl* SendScoreV1)(const IRScoreV1& score) = nullptr;
+	FetchRankStatus(__cdecl* FetchResultRankV1)(const IRRankQueryV1& query, IRRankResultV1& out) = nullptr;
+	FetchRankStatus(__cdecl* FetchChartStatsV1)(const IRRankQueryV1& query, IRChartStatsV1& out) = nullptr;
+	FetchRankStatus(__cdecl* FetchLeaderboardV1)(const IRRankQueryV1& query, IRLeaderboardResultV1& out, int limit, int offset) = nullptr;
+	FetchRankStatus(__cdecl* FetchRivalChartV1)(int rivalPlayerId, const IRRankQueryV1& query, IRRivalChartV1& out) = nullptr;
+	void(__cdecl* GetProviderMetaV1)(IRProviderMetaV1& out) = nullptr;
+	FetchRankStatus(__cdecl* FetchIrGhostV1)(const IRGhostQueryV1& query, IRGhostResultV1& out) = nullptr;
 };
