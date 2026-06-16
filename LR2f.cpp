6723287b86d8ac3da975e5938c8d1bb58c85db09
GOMazk <gommazuk@naver.com>
2026-06-16 22:13:58 +0900

    following RE issue (SPtoDP)
    f7eb6f81bbc0f8675446c205b4e58a46d294e015

--- LR2f.cpp
+++ LR2f.cpp
@@ -25511,6 +25511,7 @@ int DPsplitLaneScratch(LaneStruct *lane, int start, CHARTCONVERTER *cc) {
 					}
 				}
 				else {
+					lane->notes[scratchNoteID].op += 10;
 					if (cc->assist2p == 0) {
 						for (int j = scratchNoteID; j > 0; j--) {
 							if (lane->notes[j].op == 2) break;
@@ -25572,7 +25573,7 @@ int SPtoDP(LaneStruct *lane, int baseNoteID, CHARTCONVERTER *cc) {
 		if (cc->noteCountPerLane[i]) cc->laneCount++;
 	}
 
-	qsort(&cc->arr1, 1296, sizeof(cc->arr1), CMP_CCARRbyCount);
+	qsort(&cc->arr1, 1296, sizeof(struct_0x14), CMP_CCARRbyCount);
 
 	bool fA = false, fB = false;
 	if (cc->arr2[0].count && cc->arr1[0].count&& ((cc->arr1[0].ID == cc->arr2[0].ID && (cc->arr1[1].ID == cc->arr2[1].ID || cc->arr1[1].ID == cc->arr2[2].ID || cc->arr1[2].ID == cc->arr2[1].ID || cc->arr1[2].ID == cc->arr2[2].ID)) 
@@ -25590,7 +25591,7 @@ int SPtoDP(LaneStruct *lane, int baseNoteID, CHARTCONVERTER *cc) {
 		cc->unk14428 = 0;
 	}
 
-	qsort(&cc->arr2,1296,sizeof(cc->arr2), CMP_CCARRbyID);
+	qsort(&cc->arr2,1296,sizeof(struct_0x14), CMP_CCARRbyID);
 	if (fA) {
 		for (int i = 0; i < 1296; i++) {
 			if (cc->arr2[i].field3_0xc == 0) cc->arr2[i].field3_0xc = 1;
@@ -25634,7 +25635,7 @@ int SPtoDP(LaneStruct *lane, int baseNoteID, CHARTCONVERTER *cc) {
 		cc->arr2[i].field3_0xc = cc->arr1[i].field3_0xc;
 	}
 
-	qsort(&cc->arr1, 1296, sizeof(cc->arr1), CMP_CCARRbyID);
+	qsort(&cc->arr1, 1296, sizeof(struct_0x14), CMP_CCARRbyID);
 	for (int i = baseNoteID + 1; i < lane->count; i++) {
 
 		if (lane->notes[i].op == 2) break;
