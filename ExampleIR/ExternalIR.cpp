8859ddb58051e1f290f8c6bd4d7cf6499c722494
luxurya111 <wasd33@gmail.com>
2026-06-22 20:21:11 +0900

    refactor(customir): fix GetGhost example; fixed to make rseed work

--- ExampleIR/ExternalIR.cpp
+++ ExampleIR/ExternalIR.cpp
@@ -97,9 +97,9 @@ static openlr2::GetStatus GetGhost(const char* songHash, openlr2::GhostMode mode
         return openlr2::GetStatus::Ok;
     }
     out.displayName = "EXAMPLE";
-    out.ghostData = "#EXAMPLE,1,0,LXZ,"; // this ghost will do almost perfect play.
+    out.ghostData = "#EXAMPLE,21,2179,LXZ,"; // this ghost will do almost perfect play.
     out.gauge = openlr2::Gauge::Survival;
-    out.randomOption = { openlr2::Random::No, openlr2::Random::No };
+    out.randomOption = { openlr2::Random::Random, openlr2::Random::No };
     out.dpflip = false;
     out.rseed = 2179; // 1357246
     return openlr2::GetStatus::Ok;
