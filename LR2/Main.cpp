GOMazk/OpenLR2

commit 449de13e6ccac5d28d9c70b7b3de9faafa463005

feat(songselect): apply LR2IR rankings even if LR2IR is offline
For old players moving from LR2IR to CustomIR (e.g. Stellaverse IR),
this lets them see old rankings while they haven't played the song on
the new IR.

LR2/Main.cpp
@@ -529,6 +529,8 @@ int main(int argc, char** argv) {
 		gs.net.IR_ID = gs.gameplay.playerstat.irid;
 		if (gs.net.LR2IR_Login(gs.cmd_directplay) == 1) {
 			SaveIRID(gs.net.rankingData.myID, gs.config.player.id);
+		} else {
+			gs.net.rankingData.myID = gs.net.IR_ID;
 		}
 		printfDx(gs.net.request_result);
 		ErrorLogAdd(gs.net.request_result);

LR2/Scene02_Songselect.cpp
@@ -1596,6 +1596,10 @@ static void ThreadProc_RankingAutoUpdate(game* g) {
 	}
 
 	bool isIR2 = g->net.IRstatus == 2;
+	if (!g->net.isOnline) {
+		if (!isIR2) g->net.IRstatus = 0;
+		return;
+	}
 
 	if (!g->config.network.autoupdate) {
 		if (!isIR2) g->net.IRstatus = 0;
@@ -1810,12 +1814,10 @@ int ProcS_Select(game *g_) {
 					return 1;
 				}
 			}
-			if (g.net.isOnline) {
-				g.net.IRstatus = 1;
-				g.net.hHandle = std::jthread(ThreadProc_RankingAutoUpdate, &g);
-				SetObjectStrings_SongSelect(&g);
-				return 1;
-			}
+			g.net.IRstatus = 1;
+			g.net.hHandle = std::jthread(ThreadProc_RankingAutoUpdate, &g);
+			SetObjectStrings_SongSelect(&g);
+			return 1;
 		}
 	}
 	g.net.IRstatus = 0;

commit 1f576e795c935bf8da319e5e42f4a05d7ee1e1da

feat(main): fill NETWORK state even if LR2IR is disabled
This continues on the previous patch, making the ranking placement show
even if LR2IR is disabled in the launcher.

Fixes: 449de13e6cca ("feat(songselect): apply LR2IR rankings even if LR2IR is offline")

LR2/Main.cpp
@@ -578,19 +578,20 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	if (gs.config.network.lr2ir == 1 && gs.is_starter == 0 && gs.cmd_nosave == 0) {
+	if (gs.is_starter == 0 && gs.cmd_nosave == 0) {
 		gs.net.IR_pass = gs.config.player.pass;
 		gs.net.IR_name = gs.config.player.id;
 		gs.net.IR_passMD5 = MD5str(gs.config.player.pass);
 		gs.net.getRival = gs.config.network.getRival;
 		gs.net.IR_ID = gs.gameplay.playerstat.irid;
-		if (gs.net.LR2IR_Login(gs.cmd_directplay) == 1) {
-			SaveIRID(gs.net.rankingData.myID, gs.config.player.id);
-		} else {
-			gs.net.rankingData.myID = gs.net.IR_ID;
+		gs.net.rankingData.myID = gs.net.IR_ID;
+		if (gs.config.network.lr2ir == 1) {
+			if (gs.net.LR2IR_Login(gs.cmd_directplay) == 1) {
+				SaveIRID(gs.net.rankingData.myID, gs.config.player.id);
+			}
+			printfDx(gs.net.request_result);
+			ErrorLogAdd(gs.net.request_result);
 		}
-		printfDx(gs.net.request_result);
-		ErrorLogAdd(gs.net.request_result);
 	}
 
 	memcpy(gs.config.jukebox.rival, gs.net.rivals, 4 * 20);